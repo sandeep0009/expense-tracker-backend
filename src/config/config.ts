775475b31a39960c07409f9b25eae05ec1213520
sandeep0009 <500sandeepks@gmail.com>
2025-02-02 03:09:38 +0530

    fix:fix port

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 import dotenv from "dotenv"
 dotenv.config();
 export const JWT_KEY=process.env.JWT_PASSKEY || "random@123";
-export const PORT=process.env.PORT || 8080;
+export const PORT=process.env.PORT || 3000;
