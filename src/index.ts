e35ff25163a06887eb8ee8ad3b79a2d5cf6b29b9
sandeep0009 <500sandeepks@gmail.com>
2025-02-02 01:08:53 +0530

    fixed

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import router from "./routes";
 import './types'
 const app=express();
 
-app.use(cors());
+app.use(
+    cors({
+      origin: ["https://expense-tracker-frontend-git-master-sandeep0009s-projects.vercel.app"],
+      methods: ["GET", "POST", "PUT", "DELETE"], 
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true, 
+    })
+  );
 app.use(express.json());
 app.use(router);
 
